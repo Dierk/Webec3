604cba78b8285b5b5302254b60cd4aac62a4797a
Dierk Koenig <dierk.koenig@canoo.com>
2018-04-11 14:07:56 +0200

    after lecture 7

--- grails-app/init/mvc/BootStrap.groovy
+++ grails-app/init/mvc/BootStrap.groovy
@@ -3,6 +3,18 @@ package mvc
 class BootStrap {
 
     def init = { servletContext ->
+
+        Person dierk = new Person(firstName: "Dierk", lastName: "König")
+            .save(failOnError:true)
+        Person dieter = new Person(firstName: "Dieter", lastName: "Holz")
+            .save(failOnError:true)
+        Room bright = new Room(description: "1.331", capacity: 40,)
+            .save(failOnError:true)
+        new Booking(room: bright, booker: dieter,
+                    day: new Date() + 1, timeslot: "Vormittag"
+        ).save(failOnError:true)
+
+
     }
     def destroy = {
     }
