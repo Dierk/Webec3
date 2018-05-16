2e7cdcbc9659a2e0f84aa01cc7a8d42b5fb5df35
Dierk Koenig <dierk.koenig@canoo.com>
2018-05-16 08:59:39 +0200

    add booking/foo to display request header names and user-agent value

--- grails-app/controllers/mvc/BookingController.groovy
+++ grails-app/controllers/mvc/BookingController.groovy
@@ -5,17 +5,14 @@ class BookingController {
     static scaffold = Booking
 
     def foo() {
-//        Person dierk = Person.findByFirstName("Dierk")
-//        List<Booking> dierksBookings = Booking.findAllByBooker(dierk)
-//        List<Room> rooms = []
-//        for(Booking booking : dierksBookings) {
-//            rooms.add(booking.getRoom())
-//        }
-//        def rooms = Booking.findAllByBooker(dierk)*.room
 
-        Room one    = Room.findByDescription("6.2H03");
-        def bookers = Booking.findAllByRoom(one)*.booker.unique()
+        def msg = ""
+        for ( name in request.getHeaderNames()) {
+            msg += name + " "
+        }
+        msg += request.getHeader("user-agent")
+        println msg
 
-        respond bookers
+        render text: msg
     }
 }
